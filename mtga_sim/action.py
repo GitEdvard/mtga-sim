42cfc6c25e74c3685f0ffa800fc8e2eaba5aa8a9
Edvard Englund <edvard.englund@medsci.uu.se>
2021-04-23 11:35:49 +0200

    Move Action base class to separate module

---
+++ mtga_sim/action.py
@@ -0,0 +1,51 @@
+import abc
+import sys
+import inspect
+
+
+class Action(abc.ABC):
+    def __init__(self, creature):
+        self.creature = creature
+        self.legal = False
+
+    @classmethod
+    @abc.abstractmethod
+    def action_index(cls):
+        pass
+
+    @property
+    def power(self):
+        return self.creature.power
+
+    @property
+    def toughness(self):
+        return self.creature.toughness
+
+    @classmethod
+    def number_actions(cls):
+        classes = cls._get_subclasses()
+        return len([c for c in classes])
+
+    @property
+    def is_legal(self):
+        return self.legal
+
+    @classmethod
+    def _get_subclasses(cls):
+        ret = list()
+        """Yield the classes in module ``mod`` that inherit from ``cls``"""
+        current_module = sys.modules[cls.__module__]
+        for name, obj in inspect.getmembers(current_module):
+            if inspect.isclass(obj) and issubclass(obj, cls) and obj != cls:
+                ret.append(obj)
+
+        return ret
+
+    def __repr__(self):
+        return self.__class__.__name__
+
+    @classmethod
+    def instantiate(cls, action_number, creature):
+        for clazz in cls._get_subclasses():
+            if clazz.action_index() == action_number:
+                return clazz(creature)
